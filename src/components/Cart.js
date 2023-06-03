0846dd186f115227496e56107b076ea61670d8f1
Dainius Strazdas <dainius19961996@gmail.com>
2023-06-03 18:41:27 +0300

    Implement feature where in cart you can adjust added products quantity

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,18 +9,49 @@ import CartItem from "./CartItem";
 
 const Cart = () => {
   const [cart, setCart] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getCartContent();
   }, []);
 
   const getCartContent = async () => {
+    setLoading(true);
     try {
       const cartData = await commerce.cart.retrieve();
       setCart(cartData);
-      console.log(cartData);
     } catch (error) {
       console.error("Error retrieving cart content", error);
+    }
+    setLoading(false);
+  };
+
+  const handleUpdateCart = async () => {
+    setLoading(true);
+    try {
+      const cartData = await commerce.cart.retrieve();
+      setCart(cartData);
+    } catch (error) {
+      console.error("Error updating cart", error);
+    }
+    setLoading(false);
+  };
+
+  const handleSubtract = async (line_item_id, item_quantity) => {
+    try {
+      await commerce.cart.update(line_item_id, { quantity: item_quantity - 1 });
+      handleUpdateCart();
+    } catch (error) {
+      console.error("Error subtracting item from cart", error);
+    }
+  };
+
+  const handleAdd = async (line_item_id, item_quantity) => {
+    try {
+      await commerce.cart.update(line_item_id, { quantity: item_quantity + 1 });
+      handleUpdateCart();
+    } catch (error) {
+      console.error("Error adding item to cart", error);
     }
   };
 
@@ -29,8 +60,16 @@ const Cart = () => {
       <Nav />
       <div className="main-container">
         <div className="spacer"></div>
-        {cart && <CartItem cart={cart} />}{" "}
-        {/* Render CartItem only if cart is not null */}
+        {loading
+          ? "loading..."
+          : cart && (
+              <CartItem
+                cart={cart}
+                handleSubtract={handleSubtract}
+                handleAdd={handleAdd}
+                loading={loading}
+              />
+            )}
       </div>
       <Footer />
     </>
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { commerce } from "../lib/commerce";
 
 import "../styles/cart.css";
 
-const CartItem = ({ cart }) => {
+const CartItem = ({ cart, handleSubtract, handleAdd, loading }) => {
   const { line_items } = cart;
 
   return (
@@ -11,16 +12,26 @@ const CartItem = ({ cart }) => {
         <strong className="cart__title">Cart</strong>
         {line_items.map((item) => (
           <div key={item.id} className="cart__details">
-            <div className="cart__image">
-              <img src={item.image.url} alt={item.name} />
+            <div className="img__AndName">
+              <div className="cart__image">
+                <img src={item.image.url} alt={item.name} />
+              </div>
+              <span className="cart__name">{item.name}</span>
             </div>
-            <span className="cart__name">{item.name}</span>
             <span className="cart__price">
-              {/*THIS WHERE WILL BE AMOUNT OF PRODUCTS, WHERE U CAN ADD OR MINUS*/}
+              <button
+                onClick={() => {
+                  handleSubtract(item.id, item.quantity);
+                }}
+              >
+                -
+              </button>
+              {loading ? "Loading" : item.quantity}
+              <button onClick={() => handleAdd(item.id, item.quantity)}>
+                +
+              </button>
             </span>
-            <span className="cart__subtotal">
-              Subtotal: {item.line_total.raw} €
-            </span>
+            <span className="cart__subtotal">{item.line_total.raw} €</span>
           </div>
         ))}
       </div>
