1b269097e89c401b1813e1bed28d9eeba0fac791
Dainius Strazdas <dainius19961996@gmail.com>
2023-06-05 01:58:59 +0300

    Added function to remove item from cart, also adjusted state for each item, when pressing - or +

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { commerce } from "../lib/commerce";
-
 import "../styles/cart.css";
 
 import Nav from "./Nav";
@@ -9,49 +8,63 @@ import CartItem from "./CartItem";
 
 const Cart = () => {
   const [cart, setCart] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loadingItems, setLoadingItems] = useState([]);
+  const [loadingCart, setLoadingCart] = useState(false);
 
   useEffect(() => {
-    getCartContent();
+    handleUpdateCart();
   }, []);
 
-  const getCartContent = async () => {
-    setLoading(true);
-    try {
-      const cartData = await commerce.cart.retrieve();
-      setCart(cartData);
-    } catch (error) {
-      console.error("Error retrieving cart content", error);
-    }
-    setLoading(false);
-  };
-
   const handleUpdateCart = async () => {
-    setLoading(true);
+    setLoadingCart(true);
     try {
       const cartData = await commerce.cart.retrieve();
       setCart(cartData);
     } catch (error) {
       console.error("Error updating cart", error);
     }
-    setLoading(false);
+    setLoadingCart(false);
   };
 
   const handleSubtract = async (line_item_id, item_quantity) => {
+    setLoadingItems((prevLoadingItems) => [...prevLoadingItems, line_item_id]);
     try {
       await commerce.cart.update(line_item_id, { quantity: item_quantity - 1 });
       handleUpdateCart();
     } catch (error) {
       console.error("Error subtracting item from cart", error);
+    } finally {
+      setLoadingItems((prevLoadingItems) =>
+        prevLoadingItems.filter((id) => id !== line_item_id)
+      );
     }
   };
 
   const handleAdd = async (line_item_id, item_quantity) => {
+    setLoadingItems((prevLoadingItems) => [...prevLoadingItems, line_item_id]);
     try {
       await commerce.cart.update(line_item_id, { quantity: item_quantity + 1 });
       handleUpdateCart();
     } catch (error) {
       console.error("Error adding item to cart", error);
+    } finally {
+      setLoadingItems((prevLoadingItems) =>
+        prevLoadingItems.filter((id) => id !== line_item_id)
+      );
+    }
+  };
+
+  const handleRemoveFromCart = async (line_item_id) => {
+    setLoadingItems((prevLoadingItems) => [...prevLoadingItems, line_item_id]);
+    try {
+      await commerce.cart.remove(line_item_id);
+      handleUpdateCart();
+    } catch (error) {
+      console.error("Error removing item from cart", error);
+    } finally {
+      setLoadingItems((prevLoadingItems) =>
+        prevLoadingItems.filter((id) => id !== line_item_id)
+      );
     }
   };
 
@@ -60,16 +73,15 @@ const Cart = () => {
       <Nav />
       <div className="main-container">
         <div className="spacer"></div>
-        {loading
-          ? "loading..."
-          : cart && (
-              <CartItem
-                cart={cart}
-                handleSubtract={handleSubtract}
-                handleAdd={handleAdd}
-                loading={loading}
-              />
-            )}
+        {cart && (
+          <CartItem
+            cart={cart}
+            handleSubtract={handleSubtract}
+            handleAdd={handleAdd}
+            handleRemove={handleRemoveFromCart}
+            loadingItems={loadingItems}
+          />
+        )}
       </div>
       <Footer />
     </>
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,39 +1,47 @@
 import React from "react";
-import { commerce } from "../lib/commerce";
-
 import "../styles/cart.css";
 
-const CartItem = ({ cart, handleSubtract, handleAdd, loading }) => {
+const CartItem = ({
+  cart,
+  handleSubtract,
+  handleAdd,
+  handleRemove,
+  loadingItems,
+}) => {
   const { line_items } = cart;
 
   return (
     <>
       <div className="cart__item">
         <strong className="cart__title">Cart</strong>
-        {line_items.map((item) => (
-          <div key={item.id} className="cart__details">
-            <div className="img__AndName">
-              <div className="cart__image">
-                <img src={item.image.url} alt={item.name} />
+        {line_items.map((item) => {
+          const isLoading = loadingItems.includes(item.id);
+
+          return (
+            <div key={item.id} className="cart__details">
+              <div className="img__AndName">
+                <div className="cart__image">
+                  <img src={item.image.url} alt={item.name} />
+                </div>
+                <span className="cart__name">{item.name}</span>
               </div>
-              <span className="cart__name">{item.name}</span>
-            </div>
-            <span className="cart__price">
+              <span className="cart__price">
+                <button onClick={() => handleSubtract(item.id, item.quantity)}>
+                  -
+                </button>
+                {isLoading ? "Loading" : item.quantity}
+                <button onClick={() => handleAdd(item.id, item.quantity)}>
+                  +
+                </button>
+              </span>
+              <span className="cart__subtotal">{item.line_total.raw} €</span>
               <button
-                onClick={() => {
-                  handleSubtract(item.id, item.quantity);
-                }}
-              >
-                -
-              </button>
-              {loading ? "Loading" : item.quantity}
-              <button onClick={() => handleAdd(item.id, item.quantity)}>
-                +
-              </button>
-            </span>
-            <span className="cart__subtotal">{item.line_total.raw} €</span>
-          </div>
-        ))}
+                onClick={() => handleRemove(item.id)}
+                className="cart__remove__item"
+              ></button>
+            </div>
+          );
+        })}
       </div>
       <div className="spacer"></div>
     </>
