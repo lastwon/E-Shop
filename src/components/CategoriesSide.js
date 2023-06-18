4cea821e92866c1a3228695131d66b73e45fbe3f
Dainius Strazdas <dainius19961996@gmail.com>
2023-06-19 01:58:56 +0300

    Added backdrop when open sidebar

--- src/components/CategoriesSide.js
+++ src/components/CategoriesSide.js
@@ -9,36 +9,39 @@ import AllCategoriesSide from "./AllCategoriesSide";
 
 const CategoriesSide = ({ side }) => {
   return (
-    <div className="categories__side__menu">
-      <div className="top-bar">
-        <div className="login-message">
-          <BiUser
-            style={{
-              width: "32px",
-              height: "auto",
-              padding: "8px 5px 10px 5px",
-              color: "#fff",
-            }}
-          />
-          <Link to={"/"}>
-            Hello, <u>log in here!</u>
-          </Link>
+    <>
+      <div className="categories__side__menu">
+        <div className="top-bar">
+          <div className="login-message">
+            <BiUser
+              style={{
+                width: "32px",
+                height: "auto",
+                padding: "8px 5px 10px 5px",
+                color: "#fff",
+              }}
+            />
+            <Link to={"/"}>
+              Hello, <u>log in here!</u>
+            </Link>
+          </div>
+          <div className="top-bar-quit">
+            <AiOutlineClose
+              style={{
+                width: "30px",
+                height: "auto",
+                color: "#fff",
+              }}
+              onClick={side}
+            />
+          </div>
         </div>
-        <div className="top-bar-quit">
-          <AiOutlineClose
-            style={{
-              width: "30px",
-              height: "auto",
-              color: "#fff",
-            }}
-            onClick={side}
-          />
+        <div className="categories-bar">
+          <AllCategoriesSide side={side} />
         </div>
       </div>
-      <div className="categories-bar">
-        <AllCategoriesSide side={side} />
-      </div>
-    </div>
+      <div className="backdrop"></div>
+    </>
   );
 };
 
