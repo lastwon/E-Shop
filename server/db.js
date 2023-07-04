ea9a0a4025d03c4b62f5c77793fd679331f3027f
Dainius Strazdas <dainius19961996@gmail.com>
2023-07-04 20:42:22 +0300

    Adjusted database data

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ import mysql from "mysql";
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "45.84.207.0",
+  user: "u748648148_eshop",
+  password: "Testing123",
+  database: "u748648148_eshop",
 });
 
 export default pool;
