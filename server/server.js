4a97cc2dfef66692035eb6be3be1d0b893cbe0a0
Dainius Strazdas <dainius19961996@gmail.com>
2023-07-04 14:50:56 +0300

    heroku commit

--- server/server.js
+++ server/server.js
@@ -1,32 +1,33 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import path from "path";
 import pool from "./db.js";
+import path from "path";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Serve static files from the React build
-app.use(express.static(path.join(__dirname, "../build")));
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, "..", "build")));
 
-app.get("/:productName", (req, res, next) => {
-  if (req.params.productName.includes(".")) {
-    next();
-  } else {
-    const { productName } = req.params;
-    const sql =
-      "SELECT * FROM comments WHERE product_name = ? ORDER BY createdAt DESC";
+const port = process.env.PORT || 8081;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 
-    pool.query(sql, [productName], (err, result) => {
-      if (err) {
-        console.error("Error retrieving comments:", err);
-        return res.status(500).json({ message: "Failed to retrieve comments" });
-      }
-      return res.json(result);
-    });
-  }
+app.get("/:productName", (req, res) => {
+  const { productName } = req.params;
+  const sql =
+    "SELECT * FROM comments WHERE product_name = ? ORDER BY createdAt DESC";
+
+  pool.query(sql, [productName], (err, result) => {
+    if (err) {
+      console.error("Error retrieving comments:", err);
+      return res.status(500).json({ message: "Failed to retrieve comments" });
+    }
+    return res.json(result);
+  });
 });
 
 app.post("/:productName", (req, res) => {
@@ -52,9 +53,5 @@ app.post("/:productName", (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
-});
-
-app.listen(process.env.PORT || 8081, () => {
-  console.log(`Server is running on port ${process.env.PORT || 8081}`);
+  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
 });
