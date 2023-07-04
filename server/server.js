f3913cec4cefbd06026f5a4fa8472e5fcc66087e
Dainius Strazdas <dainius19961996@gmail.com>
2023-07-04 13:31:51 +0300

    Ready for Onrender deploy

--- server/server.js
+++ server/server.js
@@ -1,28 +1,32 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import path from "path";
 import pool from "./db.js";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.listen(8081, () => {
-  console.log("Server is listening on port 8081");
-});
+// Serve static files from the React build
+app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/:productName", (req, res) => {
-  const { productName } = req.params;
-  const sql =
-    "SELECT * FROM comments WHERE product_name = ? ORDER BY createdAt DESC";
+app.get("/:productName", (req, res, next) => {
+  if (req.params.productName.includes(".")) {
+    next();
+  } else {
+    const { productName } = req.params;
+    const sql =
+      "SELECT * FROM comments WHERE product_name = ? ORDER BY createdAt DESC";
 
-  pool.query(sql, [productName], (err, result) => {
-    if (err) {
-      console.error("Error retrieving comments:", err);
-      return res.status(500).json({ message: "Failed to retrieve comments" });
-    }
-    return res.json(result);
-  });
+    pool.query(sql, [productName], (err, result) => {
+      if (err) {
+        console.error("Error retrieving comments:", err);
+        return res.status(500).json({ message: "Failed to retrieve comments" });
+      }
+      return res.json(result);
+    });
+  }
 });
 
 app.post("/:productName", (req, res) => {
@@ -43,4 +47,14 @@ app.post("/:productName", (req, res) => {
       res.status(200).json({ message: "Comment submitted successfully" });
     }
   );
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
+app.listen(process.env.PORT || 8081, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8081}`);
 });
